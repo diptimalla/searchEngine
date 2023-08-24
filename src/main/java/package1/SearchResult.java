cb5be0840ec09b4267d3ca54d9b6751fc9c91208
diptimalla <pdaffy0@gmail.com>
2023-08-24 21:24:05 +0530

    initial commit

---
+++ src/main/java/package1/SearchResult.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class SearchResult {
+    String title;
+    String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTile(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
